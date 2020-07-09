d8646cdaa9dd82b4617dd13768d7f4d2d7aa3373
Raghavender <raghavender.n@sixt.com>
2020-07-09 21:00:21 +0530

    Fixed the password strength issue

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -5,6 +5,8 @@ import ImageHoster.model.User;
 import ImageHoster.model.UserProfile;
 import ImageHoster.service.ImageService;
 import ImageHoster.service.UserService;
+import ImageHoster.utils.StringUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -40,9 +42,24 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user,Model model) {
+
+    	String password=user.getPassword();
+    	
+    	if (StringUtils.isValidPassword(password)) {
+    		userService.registerUser(user);
+			return "redirect:/users/login";
+		}else {
+			String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+			model.addAttribute("passwordTypeError", error);
+		}
+			
+    	user = new User();
+        UserProfile profile = new UserProfile();
+        user.setProfile(profile);
+        model.addAttribute("User", user);
+    	return "users/registration";
+    	
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
