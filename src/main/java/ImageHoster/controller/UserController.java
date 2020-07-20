057d9466f4b48a5644a9275968772c9437f4b039
Raghavender <raghavender.n@sixt.com>
2020-07-20 19:18:20 +0530

    Fixed the test cases

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -48,7 +48,7 @@ public class UserController {
     	
     	if (StringUtils.isValidPassword(password)) {
     		userService.registerUser(user);
-			return "redirect:/users/login";
+			return "users/login";
 		}else {
 			String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
 			model.addAttribute("passwordTypeError", error);
